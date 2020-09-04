90474bb30bc113cbd6f160bbcdf4520d8958a1a5
Jesse Fredrickson <jesse@Jesses-MacBook-Pro.local>
2020-09-03 22:51:52 -0400

    adopted new notebook methods for hu scaling, windowing, and masking

--- scripts/submission_scripts/pre_proc.py
+++ scripts/submission_scripts/pre_proc.py
@@ -12,218 +12,103 @@ from tqdm import tqdm  # for progress bars
 from scipy.ndimage import gaussian_filter  # for lungmasking
 
 
-def make_lungmask(img, display=False):
-    """
-    - Standardize the pixel value by subtracting the mean and dividing by the standard deviation
-    - Identify the proper threshold by creating 2 KMeans clusters comparing centered on soft tissue/bone vs lung/air.
-    - Use Erosion and Dilation which has the net effect of removing tiny features like pulmonary vessels or noise
-    - Identify each distinct region as separate image labels (think the magic wand in Photoshop)
-    - Using bounding boxes for each image label to identify which ones represent lung and which ones represent "every thing else"
-    - Create the masks for lung fields.
-    - Apply mask onto the original image to erase voxels outside of the lung fields.
-    INPUTS:
-        img (np.array): pixel array containing a lung
-        display (bool): plotting purposes only
-    RETURN:
-        (np.array): masked image pixel array
-    """
-    row_size = img.shape[0]
-    col_size = img.shape[1]
+def transform_to_hu(img, rescale_slope, rescale_intercept):
 
-    im_mean = np.mean(img)
-    std = np.std(img)
-    img = img - im_mean
-    img = img / std
-    # Find the average pixel value near the lungs to renormalize washed out images.
-    middle = img[
-        int(col_size / 5) : int(col_size / 5 * 4),
-        int(row_size / 5) : int(row_size / 5 * 4),
-    ]
-    im_mean = np.mean(middle)
-    im_max = np.max(img)
-    im_min = np.min(img)
-    # To improve threshold finding, I'm moving the underflow and overflow on the pixel spectrum
-    img[img == im_max] = im_mean
-    img[img == im_min] = im_mean
-    # Use Kmeans to separate foreground (soft tissue / bone) and background (lung/air)
-    kmeans = KMeans(n_clusters=2).fit(np.reshape(middle, [np.prod(middle.shape), 1]))
-    centers = sorted(kmeans.cluster_centers_.flatten())
-    threshold = np.mean(centers)
-    thresh_img = np.where(img < threshold, 1.0, 0.0)  # threshold the image
-
-    # First erode away the finer elements, then dilate to include some of the pixels surrounding the lung.
-    # We don't want to accidentally clip the lung.
-    erosion = morphology.erosion(thresh_img, np.ones([3, 3]))
-    dilation = morphology.dilation(erosion, np.ones([15, 15]))
-
-    labels = measure.label(
-        dilation
-    )  # Different labels are displayed in different colors
-    labels = (
-        labels + 1
-    )  # add 1 to every element so that the background is no longer encoded as 0
-    regions = measure.regionprops(labels)  # for some reason ignores labels marked 0
-    good_labels = []
-    n_px = len(img.flatten())
-    for prop in regions:
-        B = prop.bbox  # (min_row, min_col, max_row, max_col)
-        # region width < 90% of img
-        # region height < 90% of img
-        # min row > 15%, max row < 85%
-        # min col < 15%, max col < 85%
-        # region area is > 0.1% of img
-        if (
-            B[2] - B[0] < row_size * 0.90
-            and B[3] - B[1] < col_size * 0.90
-            and B[0] > row_size * 0.075
-            and B[2] < row_size * 0.925
-            and B[1] > col_size * 0.075
-            and B[3] < col_size * 925
-            and prop.area / n_px * 100 > 0.1
-        ):
-            good_labels.append(prop.label)
-            # print(f'prop {prop.label} area: {np.round(prop.area/n_px*100,2)}, {B}')
-    mask = np.ndarray([row_size, col_size], dtype=np.int8)
-    mask[:] = 0
-
-    #  After just the lungs are left, we do another large dilation
-    #  in order to fill in and out the lung mask
-    for N in good_labels:
-        mask = mask + np.where(labels == N, 1, 0)
-    mask = morphology.dilation(mask, np.ones([5, 5]))  # one last dilation
-
-    # final masked image
-    masked_img = mask * img
-    mask_var = np.round(np.var(mask), 4)
-    n_labels = len(good_labels)
-
-    if display:
-        fig, ax = plt.subplots(3, 2, figsize=[8, 12])
-        ax[0, 0].set_title("Original")
-        ax[0, 0].imshow(img, cmap="gray")
-        ax[0, 0].axis("off")
-        ax[0, 1].set_title("Threshold")
-        ax[0, 1].imshow(thresh_img, cmap="gray")
-        ax[0, 1].axis("off")
-        ax[1, 0].set_title("After Erosion and Dilation")
-        ax[1, 0].imshow(dilation, cmap="gray")
-        ax[1, 0].axis("off")
-        ax[1, 1].set_title("Color Labels")
-        ax[1, 1].imshow(labels)
-        ax[1, 1].axis("off")
-        ax[2, 0].set_title("Final Mask")
-        ax[2, 0].imshow(mask, cmap="gray")
-        ax[2, 0].axis("off")
-        ax[2, 1].set_title("Apply Mask on Original")
-        ax[2, 1].imshow(masked_img, cmap="gray")
-        ax[2, 1].axis("off")
-
-        plt.show()
-    return masked_img, mask_var, n_labels
+    # convert ouside pixel-values to air:
+    # I'm using <= -1000 to be sure that other defaults are captured as well
+    img[img <= -1000] = 0
+    
+    # convert to HU
+    if rescale_slope != 1:
+        img = rescale_slope * img.astype(np.float64)
+        img = img.astype(np.int16)
+        
+    img += np.int16(rescale_intercept)
+    
+    return img
 
 
-# new method using otsu's thresholding
-def make_lungmask_v2(img):
-
-    mid_dict = {}
-
-    row_size = img.shape[0]
-    col_size = img.shape[1]
-
-    # Rescale pixels to 255
-    img = img.astype(float)
-    #### need to get middle portion and normalize to it
-    # Find the average pixel value near the lungs to renormalize washed out images.
-    middle = img[
-        int(col_size / 5) : int(col_size / 5 * 4),
-        int(row_size / 5) : int(row_size / 5 * 4),
-    ]
-    im_mean = np.mean(middle)
-    im_max = np.max(img)
-    im_min = np.min(img)
-    # To improve threshold finding, I'm moving the underflow and overflow on the pixel spectrum
-    img[img == im_max] = im_mean
-    img[img == im_min] = im_mean
-
-    im_min = np.min(img)  # many are not windowed and have vals < 0
-    if im_min < 0:
-        img = img - im_min
-    img = (np.maximum(img, 0) / img.max()) * 255.0
-
-    # EXPERIMENTAL
-    img = gaussian_filter(img, sigma=1)
-
-    # otsu's binarization thresholding
-    thresh, thresh_img = threshold(
-        img.astype("uint8"), np.min(img), np.max(img), THRESH_OTSU
-    )
-    # rescale to binary and invert
-    thresh_img = np.where(thresh_img == 0, 1, 0)
-
-    mid_dict["thresholded"] = thresh_img
-
-    ###########################################
-
-    # now copy functionality from the old method to achieve a mask
-
-    # First erode away the finer elements, then dilate to include some of the pixels surrounding the lung.
-    # We don't want to accidentally clip the lung.
-    erosion = morphology.erosion(thresh_img, np.ones([3, 3]))
-    dilation = morphology.dilation(erosion, np.ones([13, 13]))
-
-    mid_dict["morph"] = dilation
-
-    labels = measure.label(
-        dilation
-    )  # Different labels are displayed in different colors
-    labels = (
-        labels + 1
-    )  # add 1 to every element so that the background is no longer encoded as 0
-    regions = measure.regionprops(labels)  # for some reason ignores labels marked 0
-    good_labels = []
-    n_px = len(img.flatten())
-    for prop in regions:
-        B = prop.bbox  # (min_row, min_col, max_row, max_col)
-        # region width < 90% of img
-        # region height < 90% of img
-        # min row > 15%, max row < 85%
-        # min col < 15%, max col < 85%
-        # region area is > 0.1% of img
-        if (
-            B[2] - B[0] < row_size * 0.80
-            and B[3] - B[1] < col_size * 0.80
-            and B[0] > row_size * 0.05
-            and B[2] < row_size * 0.95
-            and B[1] > col_size * 0.05
-            and B[3] < col_size * 0.95
-            and prop.area / n_px * 100 > 0.1
-        ):
-            good_labels.append(prop.label)
-            # print(f'prop {prop.label} area: {np.round(prop.area/n_px*100,2)}, {B}')
-    mask = np.ndarray([row_size, col_size], dtype=np.int8)
-    mask[:] = 0
-
-    #  After just the lungs are left, we do another large dilation
-    #  in order to fill in and out the lung mask
-    for N in good_labels:
-        mask = mask + np.where(labels == N, 1, 0)
-    mask = morphology.dilation(mask, np.ones([5, 5]))  # one last dilation
-
-    # final masked image
-    masked_img = mask * img
-    mask_var = np.round(np.var(mask), 4)
-    n_labels = len(good_labels)
-
-    return masked_img, mask_var, n_labels, mid_dict
+def set_manual_window(hu_image, custom_center=-500, custom_width=1000):
+    w_image = hu_image.copy()
+    min_value = custom_center - (custom_width/2)
+    max_value = custom_center + (custom_width/2)
+    w_image[w_image < min_value] = min_value
+    w_image[w_image > max_value] = max_value
+    return w_image
 
 
-def process_data(
+def lung_mask(img, manual_threshold=320):
+    # Returns the masked portion of the image showing just the lungs
+    # With -320 we are separating between lungs (-700) /air (-1000) and tissue with values close to water (0).
+    
+    binary_image = np.array(img > -manual_threshold, dtype=np.int8)+1
+    labels = measure.label(binary_image)
+    
+    background_label_1 = labels[0,0]
+    background_label_2 = labels[0,-1]
+    background_label_3 = labels[-1,0]
+    background_label_4 = labels[-1,-1]
+
+    #Fill the air around the person
+    binary_image[background_label_1 == labels] = 2
+    binary_image[background_label_2 == labels] = 2
+    binary_image[background_label_3 == labels] = 2
+    binary_image[background_label_4 == labels] = 2
+
+    # Morph - closing =  dilation followed by erosion
+    kernel = morphology.disk(4)
+    binary_image = morphology.closing(binary_image, kernel)
+
+    binary_image -= 1 # Make the image actual binary
+    binary_image = 1-binary_image # Invert it, lungs are now 1
+    
+    masked_img = binary_image.copy() * img
+    
+    return masked_img
+
+
+def crop_and_resize(img, crop_factor, img_px_size):
+    x, y = img.shape
+    xmin = int(np.floor(x * crop_factor))
+    xmax = int(np.floor(x * (1 - crop_factor)))
+    ymin = int(np.floor(y * crop_factor))
+    ymax = int(np.floor(y * (1 - crop_factor)))
+    img = resize(np.array(img[xmin:xmax, ymin:ymax]), (img_px_size, img_px_size))
+    return img
+
+
+def resize_volume(slices, hm_slices):
+    '''
+    takes in a list of slices, then resized it into a common depth
+    '''
+    # try opencv.resize on a different axis
+    slices = np.array(slices)
+    # initialze resized array, then modify each slice along second axis
+    resized_slices = np.zeros(
+        shape=(hm_slices, img_px_size, img_px_size)
+    )  # (8, 64, 64)
+    for i in range(resized_slices.shape[1]):
+        resized_slices[:, i, :] = resize(slices[:, i, :], (img_px_size, hm_slices))
+        # interpolation methods are: (https://www.tutorialkart.com/opencv/python/opencv-python-resize-image/)
+        # INTER_NEAREST – a nearest-neighbor interpolation
+        # INTER_LINEAR – a bilinear interpolation (used by default)
+        # INTER_AREA – resampling using pixel area relation.
+        #   It may be a preferred method for image decimation, as it gives moire’-free results.
+        #   But when the image is zoomed, it is similar to the INTER_NEAREST method.
+        # INTER_CUBIC – a bicubic interpolation over 4×4 pixel neighborhood
+        # INTER_LANCZOS4 – a Lanczos interpolation over 8×8 pixel neighborhood
+
+    return resized_slices
+
+
+def process_patient(
     patient,
     patient_history_df,
     img_px_size=32,
     hm_slices=8,
     verbose=False,
     data_dir="./data/train/",
+    crop_factor=0,
 ):
     """
     Function to read in all of the DICOMS in a patient dir, condense arrays into aggregated chunks
@@ -232,6 +117,7 @@ def process_data(
         patient_history_df (pd.DataFrame): dataframe of tabular data associated with this patient
         img_px_size (int): resize source DICOM image array to square matrix of this shape
         hm_slices (int): number of chunks to output
+        crop_factor (float): between 0 and 1, percent of image to remove from borders of image
     RETURNS:
         img_data (np.Array): (hm_slices, img_px_size, img_px_size) array for one patient
         patient_history (pd.DataFrame): Patient tabular data, filtered to critical columns
@@ -243,75 +129,48 @@ def process_data(
         key=lambda x: int(x.ImagePositionPatient[2])
     )  # sorts DICOMS by caudial (ass) to cranial (head)
 
-    # each scan is not the same depth (number of slices), we we group the slices into chunks of size HM_SLICES
-    # and average across them to make sure the dimensionality is standardized
-    # get pixel arrays for each dicom, HU rescale at the same time
-    slices = [dicom.pixel_array for dicom in dicoms]
+    slices = []
+    for dicom in tqdm(dicoms):
+        # grab information from dicoms for later
+        img = dicom.pixel_array
+        rescale_intercept = dicom.RescaleIntercept
+        rescale_slope = dicom.RescaleSlope
 
-    # lung masking
-    masked_slices = []
-    for each_slice in tqdm(slices):
-        masked_img, mask_var, n_labels, _ = make_lungmask_v2(each_slice)
-        # check and see if this slice is worth keeping (contains lungs, did a good job of masking them)
-        if mask_var > 0.04 and n_labels < 10:
-            masked_slices.append(masked_img)
+        # rescale HU
+        hu_scaled_img = transform_to_hu(img=img, rescale_slope=rescale_slope, rescale_intercept=rescale_intercept)
+        # window
+        windowed_img = set_manual_window(hu_scaled_img)
+        # mask slice
+        masked_img = lung_mask(windowed_img)
+        # resize to common dimensions, optionally center crop
+        resized_img = crop_and_resize(masked_img, crop_factor=crop_factor, img_px_size=img_px_size)
+        # add finished image to list of slices
+        slices.append(resized_img)
 
-    # resize each pixel array - slices changed type to array here. Start as 512 x 512
-    resized_masked_slices = []
-    for each_slice in masked_slices:
-        CROP_FACTOR = 0.2
-        x, y = each_slice.shape
-        xmin = int(np.floor(x * CROP_FACTOR))
-        xmax = int(np.floor(x * (1 - CROP_FACTOR)))
-        ymin = int(np.floor(y * CROP_FACTOR))
-        ymax = int(np.floor(y * (1 - CROP_FACTOR)))
-        resized_masked_slices.append(
-            resize(
-                np.array(each_slice[xmin:xmax, ymin:ymax]), (img_px_size, img_px_size)
-            )
-        )
-    # masked_slices = [resize(np.array(each_slice), (img_px_size, img_px_size)) for each_slice in masked_slices]
-
-    # try opencv.resize on a different axis
-    resized_masked_slices = np.array(resized_masked_slices)
-    # initialze resized array, then modify each slice along second axis
-    chunked_slices = np.zeros(
-        shape=(hm_slices, img_px_size, img_px_size)
-    )  # (8, 64, 64)
-    for i in range(chunked_slices.shape[1]):
-        chunked_slices[:, i, :] = resize(
-            resized_masked_slices[:, i, :], (img_px_size, hm_slices)
-        )
-        # interpolation methods are: (https://www.tutorialkart.com/opencv/python/opencv-python-resize-image/)
-        # INTER_NEAREST – a nearest-neighbor interpolation
-        # INTER_LINEAR – a bilinear interpolation (used by default)
-        # INTER_AREA – resampling using pixel area relation.
-        #   It may be a preferred method for image decimation, as it gives moire’-free results.
-        #   But when the image is zoomed, it is similar to the INTER_NEAREST method.
-        # INTER_CUBIC – a bicubic interpolation over 4×4 pixel neighborhood
-        # INTER_LANCZOS4 – a Lanczos interpolation over 8×8 pixel neighborhood
+    # combine all slices into a volume and reshape into common volume
+    resized_volume = resize_volume(slices, hm_slices)
 
     relevant_side_info = patient_history_df[["Patient", "Weeks", "FVC", "Percent"]]
 
     if verbose:
         print(
-            f"Patient {patient}; max: {chunked_slices.max()}, min: {chunked_slices.min()}"
+            f"Patient {patient}"
         )
 
-    return chunked_slices, relevant_side_info
+    return resized_volume, relevant_side_info
 
 
 def read_in_data(
-    data_dir="./data/train/",
+    csv_path,
+    patient_dir,
     img_px_size=32,
     slice_count=8,
     verbose=False,
-    csv_file="/kaggle/input/osic-pulmonary-fibrosis-progression/train.csv",
 ):
     """
     Function to ___
     INPUTS:
-        data_dir (str): folder location of training DICOMS patient folders
+        csv_path (str): file location of train csv
         img_px_size (int): resize source DICOM image array to square matrix of this shape
         hm_slices (int): number of chunks to condense each patient DICOMs into
     RETURNS:
@@ -319,9 +178,9 @@ def read_in_data(
     """
 
     patients = os.listdir(
-        "./data/train/"
+        patient_dir
     )  # list of training patient IDS (folders which contain DICOMS)
-    train_df = pd.read_csv(csv_file)  # list of training patient tabular data
+    train_df = pd.read_csv(csv_path)  # list of training patient tabular data
 
     IMG_PX_SIZE = img_px_size
     SLICE_COUNT = slice_count
@@ -337,7 +196,7 @@ def read_in_data(
         )
 
         try:
-            img_data, patient_history = process_data(
+            img_data, patient_history = process_patient(
                 patient,
                 patient_history_df,
                 img_px_size=IMG_PX_SIZE,
@@ -363,10 +222,17 @@ def save_to_disk(data, img_px_size=32, slice_count=8):
 
 
 def main():
+    LOCAL_RUN = True # set this to False for submission!
     img_px_size = 64
     slice_count = 8
+    local_csv_path = "./data/train.csv"
+    kaggle_csv_path = "/kaggle/input/osic-pulmonary-fibrosis-progression/train.csv"
+    local_patient_dir = "./data/train/"
+    kaggle_patient_dir = "./data/train/"
+
     patient_data = read_in_data(
-        data_dir="./data/processed_data/",
+        csv_path=local_csv_path if LOCAL_RUN else kaggle_csv_path,
+        patient_dir=local_patient_dir if LOCAL_RUN else kaggle_patient_dir,
         img_px_size=img_px_size,
         slice_count=slice_count,
         verbose=True,
