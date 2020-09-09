281b6c8b4492dc700e9a6b2ab11f623a7c500c49
Vinny DeGenova <vdegenova@gmail.com>
2020-09-08 21:39:18 -0400

    total submission script now supports training only runs

--- scripts/submission_scripts/total_submission.py
+++ scripts/submission_scripts/total_submission.py
@@ -21,8 +21,22 @@ from inference import infer
 
 
 def main():
+    # flags to assist in kaggle versus development
+    is_kaggle_submission = False    # is this a final submission run for kaggle
+    do_preproc = False              # should we read from disc and perform preprocessing
+    # is this a run on training data (for offline eval)
+    eval_on_training = True
 
-    is_kaggle_submission = False
+    # setting up the respective numpy files for training/test to use if do_preproc is false
+    if do_preproc:
+        preprocessed_training_npy = None
+        preprocessed_test_npy = None
+    else:
+        preprocessed_training_npy = "/Users/vdegenova/Projects/osic_kaggle/working/170-images-with_ids-64-64-8-2020-09-08T10:59.npy"
+        preprocessed_test_npy = "/Users/vdegenova/Projects/osic_kaggle/working/5-images-with_ids-64-64-8-2020-09-08T10:59.npy"
+
+    if not do_preproc:
+        assert(preprocessed_training_npy is not None), "preprocessed_training_npy must be set when do_preproc is False"
 
     if is_kaggle_submission:
         # Kaggle specific file paths
@@ -43,19 +57,29 @@ def main():
         test_dir = "./data/test/"
         test_csv_dir = "./data/test.csv"
         encoding_filepath = f"{working_dir}patient_ids_to_encodings_dict"
-        output_filepath = "./working/submission.csv"
 
-        # preprocessed_training_npy = "/Users/vdegenova/Projects/osic_kaggle/working/158-images-with_ids-64-64-8-2020-09-01T21:35.npy"
-        # preprocessed_test_npy = "/Users/vdegenova/Projects/osic_kaggle/working/5-images-with_ids-64-64-8-2020-09-01T21:35.npy"
+        substr = ""
+        if eval_on_training:
+            substr += "training_only_"
+        output_filepath = f"./working/{substr}submission.csv"
 
-    # overall program flow
+    # overall program flow (submission)
     # 1. read in data                               (train and test)
     # 2. pre process data                           (train and test)
     # 3. train autoencoder                          (training data)
     # 4. embed images                               (train and test)
     # 5. train regressor models                     (training data)
     # 6. generate predictions and confidence values (test data)
-    # 7. generate output file                        (test data)
+    # 7. generate output file                       (test data)
+
+    # overall program flow (eval_on_training = true)
+    # 1. read in data                               (train)
+    # 2. pre process data                           (train)
+    # 3. train autoencoder                          (train)
+    # 4. embed images                               (train)
+    # 5. train regressor models                     (train)
+    # 6. generate predictions and confidence values (train)
+    # 7. generate output file                       (train)
 
     # constants
     img_px_size = 64
@@ -63,41 +87,48 @@ def main():
     N_AUTOENCODER_EPOCHS = 1
     N_REGRESSOR_EPOCHS = 1
 
-    ################################################
-    # 1. read in data (train and test)
-    ################################################
-    patient_training_data = read_in_data(
-        patient_dir=training_dir,
-        img_px_size=img_px_size,
-        slice_count=slice_count,
-        verbose=True,
-        csv_path=training_csv_dir,
-    )
-    patient_test_data = read_in_data(
-        patient_dir=test_dir,
-        img_px_size=img_px_size,
-        slice_count=slice_count,
-        verbose=True,
-        csv_path=test_csv_dir,
-    )
+    # we only read in data from disc if do_preproc is set to true, otherwise we use the numpy files that are set above
+    if do_preproc:
+        ################################################
+        # 1. read in data (train and test)
+        ################################################
+        patient_training_data = read_in_data(
+            patient_dir=training_dir,
+            img_px_size=img_px_size,
+            slice_count=slice_count,
+            verbose=True,
+            csv_path=training_csv_dir,
+        )
 
-    ################################################
-    # 2. pre process dicom data (train and test)
-    ################################################
-    preprocessed_training_npy = save_to_disk(
-        patient_training_data,
-        img_px_size=img_px_size,
-        slice_count=slice_count,
-        working_dir=working_dir,
-    )
-    preprocessed_test_npy = save_to_disk(
-        patient_test_data,
-        img_px_size=img_px_size,
-        slice_count=slice_count,
-        working_dir=working_dir,
-    )
-    del patient_training_data
-    del patient_test_data
+        if not eval_on_training:
+            patient_test_data = read_in_data(
+                patient_dir=test_dir,
+                img_px_size=img_px_size,
+                slice_count=slice_count,
+                verbose=True,
+                csv_path=test_csv_dir,
+            )
+
+        ################################################
+        # 2. pre process dicom data (train and test)
+        ################################################
+        preprocessed_training_npy = save_to_disk(
+            patient_training_data,
+            img_px_size=img_px_size,
+            slice_count=slice_count,
+            working_dir=working_dir,
+        )
+
+        if not eval_on_training:
+            preprocessed_test_npy = save_to_disk(
+                patient_test_data,
+                img_px_size=img_px_size,
+                slice_count=slice_count,
+                working_dir=working_dir,
+            )
+            del patient_test_data
+
+        del patient_training_data
 
     ################################################
     # 3. train autoencoder (training data)
@@ -125,26 +156,31 @@ def main():
         encoded_training_patients, encoded_val_patients
     )
 
-    test_data, test_patients = get_test_data_and_patients(preprocessed_test_npy)
-    encoded_test_patients = encode_patients(test_patients, test_data, encoder)
+    if not eval_on_training:
+        test_data, test_patients = get_test_data_and_patients(
+            preprocessed_test_npy)
+        encoded_test_patients = encode_patients(
+            test_patients, test_data, encoder)
 
     # Save embeddings as pkl of dictionary
     now = datetime.datetime.now().isoformat(timespec="minutes")
     training_encoding_path = encoding_filepath + f"-training-{now}.pkl"
-    print("{} Training Patients encoded.".format(len(all_encoded_training_patients)))
+    print("{} Training Patients encoded.".format(
+        len(all_encoded_training_patients)))
     print("Saving to {}.pkl".format(training_encoding_path))
 
     with open(training_encoding_path, "wb") as f:
         pickle.dump(all_encoded_training_patients, f, pickle.HIGHEST_PROTOCOL)
     del all_encoded_training_patients
 
-    testing_encoding_path = encoding_filepath + f"-test-{now}.pkl"
-    print("{} Test Patients encoded.".format(len(encoded_test_patients)))
-    print("Saving to {}.pkl".format(testing_encoding_path))
+    if not eval_on_training:
+        testing_encoding_path = encoding_filepath + f"-test-{now}.pkl"
+        print("{} Test Patients encoded.".format(len(encoded_test_patients)))
+        print("Saving to {}.pkl".format(testing_encoding_path))
 
-    with open(testing_encoding_path, "wb") as f:
-        pickle.dump(encoded_test_patients, f, pickle.HIGHEST_PROTOCOL)
-    del encoded_test_patients
+        with open(testing_encoding_path, "wb") as f:
+            pickle.dump(encoded_test_patients, f, pickle.HIGHEST_PROTOCOL)
+        del encoded_test_patients
 
     ################################################
     # 5. train regressor models (training data)
@@ -156,12 +192,13 @@ def main():
 
     # get, run pipeline
     no_op_attrs, num_attrs, cat_attrs, encoded_attrs = get_pipeline_selectors()
-    pipeline = get_postproc_pipeline(no_op_attrs, num_attrs, cat_attrs, encoded_attrs)
+    pipeline = get_postproc_pipeline(
+        no_op_attrs, num_attrs, cat_attrs, encoded_attrs)
     X = pipeline.fit_transform(
         X
     ).toarray()  # returns scipy.sparse.csr.csr_matrix by default
 
-    # Create regressora and  quantile regressor
+    # Create regressor and  quantile regressor
     regressor = create_dense_regressor(n_input_dims=X.shape[1])
     quantile_regressor = create_dense_regressor(
         n_input_dims=X.shape[1], quantile_regression=True
@@ -186,28 +223,44 @@ def main():
     )
 
     ################################################
-    # 6. generate predictions and confidence values (test data)
+    # 6. generate predictions and confidence values (test or train data)
+    # whether we're using test or train data here depends on the flag eval_on_training set above
     ################################################
     # first preprocess the test data for the regressor models
 
-    all_test_data = load_pickled_encodings(testing_encoding_path, test_csv_dir)
+    encodings_to_load = None
+    csv_to_load = None
 
-    min_weeks = -12
-    max_weeks = 133
-    weeks = list(range(min_weeks, max_weeks + 1, 1))
+    if not eval_on_training:
+        encodings_to_load = testing_encoding_path
+        csv_to_load = test_csv_dir
+    else:
+        encodings_to_load = training_encoding_path
+        csv_to_load = training_csv_dir
 
-    all_test_data = all_test_data.drop_duplicates(subset=["Patient"], keep="first")
+    all_test_data = load_pickled_encodings(encodings_to_load, csv_to_load)
 
-    total_df = pd.concat([all_test_data] * len(weeks))
+    if not eval_on_training:
+        all_test_data = all_test_data.drop_duplicates(
+            subset=["Patient"], keep="first")
 
-    new_weeks = []
-    n_test_patients = all_test_data.Patient.nunique()
-    for week in weeks:
-        this_week_duped = [week] * n_test_patients
-        new_weeks.extend(this_week_duped)
+        min_weeks = -12
+        max_weeks = 133
+        weeks = list(range(min_weeks, max_weeks + 1, 1))
 
-    total_df["Weeks"] = new_weeks
-    full_test_data = total_df
+        total_df = pd.concat([all_test_data] * len(weeks))
+
+        new_weeks = []
+        n_test_patients = all_test_data.Patient.nunique()
+        for week in weeks:
+            this_week_duped = [week] * n_test_patients
+            new_weeks.extend(this_week_duped)
+
+        total_df["Weeks"] = new_weeks
+        full_test_data = total_df
+    else:
+        full_test_data = all_test_data
+        new_weeks = full_test_data.Weeks.values
 
     X_test = full_test_data.drop(
         columns="FVC"
