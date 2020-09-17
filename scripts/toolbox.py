2cf1af55d972d2c3967c25252efc077e83b5b9fc
Stuart <>
2020-09-16 17:14:55 -0700

    More toolbox functions

--- scripts/toolbox.py
+++ scripts/toolbox.py
@@ -2,6 +2,7 @@
 
 import os
 import pydicom
+import tqdm
 import pandas as pd
 import numpy as np
 
@@ -9,8 +10,8 @@ def get_patient_data(csv_path, patient_ids=None):
     """Returns the medical data associated with the provided patient_id,
         or for all patients if not provided.
 
-    :param data_path: Absolute path to the csv file continaing patient data, e.g. 'train.csv'
-    :type data_path: str, required
+    :param csv_path: Absolute path to the csv file continaing patient data, e.g. 'train.csv'
+    :type csv_path: str, required
     :param patient_id: A list of patient IDs as strings used to select patient data.
         If None, selects all patients.
     :type patient_id: list, optional
@@ -61,9 +62,10 @@ def get_patient_dicoms(dicom_path, patient_ids=None, sort_slices=True):
 
     return patient_dicoms
 
-def score_prediction(real, pred, conf, min_conf=70, max_err=1000)
+
+def score_prediction(real, pred, conf, min_conf=70, max_err=1000):
     """Returns the bounded Laplace log likelihood based on prediction error and confidence
-    
+
     :param real: The real value of the data
     :type real: float, required
     :param pred: The predicted value of the data
@@ -81,3 +83,69 @@ def score_prediction(real, pred, conf, min_conf=70, max_err=1000)
     bounded_conf = np.maximum(conf, min_conf)
     bounded_err = np.minimum(np.abs(real - pred), max_err)
     return -(np.sqrt(2) * bounded_err / bounded_conf) - np.log(np.sqrt(2) * bounded_conf)
+
+def search_best_confidence(model, csv_path, dicom_path, conf_band_size=10, verbose=False):
+    """Iteratively uses :model: to make FVC predictions at different confidence levels
+        to find the confidence that provides the best average laplace log likelihood for :model:
+
+    :param model: An inference model with a model.predict interface
+    :type model: any interface-compliant class, required
+    :param csv_path: Absolute path to the csv file continaing patient data, e.g. 'train.csv'
+    :type csv_path: str, required
+    :param dicom_path: Absolute path to the directory containing the subdirs of patient DICOM images
+    :type dicom_path: str, required
+    :param conf_band_size: The size of confidence bands to test between 0 and 100.
+        Defaults to 10 (confidence deciles), to predict at 10th, 20th, etc. percentiles
+    :type conf_band_size: int, required
+    :param verbose: Controls function verbosity while searching
+    :type verbose: bool, optional
+    :return: The confidence band with the highest average laplace log likelihood for :model:
+    :rtype: int
+    """
+
+    # Load data
+    average_scores = []
+    patient_data = get_patient_data(csv_path)
+    patient_dicoms = get_patient_dicoms(dicom_path)
+
+    # Make predictions at each confidence band
+    for conf in tqdm.tqdm(range(conf_band_size, 100, conf_band_size)):
+        conf_scores = []
+        # Use DICOM dict for keys to avoid patients with bad DICOMs
+        for pid, dicoms in patient_dicoms.items():
+            bio_data = patient_data[patient_data['Patient'] == pid]
+            dicoms = np.expand_dims(dicoms, axis=-1)
+
+            # Multiple training weeks for each patient - predict & score at each
+            patient_scores = []
+            for weekly_data in bio_data.itertuples():
+                real_fvc = weekly_data.FVC
+                # Vector of per-slice predictions
+                fvc_predictions = model.predict([bio_data, dicoms]) 
+                # Select the prediction that is the :conf:'th percentile of predictions
+                pred_fvc = np.quantile(fvc_predictions, conf / 100) # q param must be between 0-1
+                patient_scores.append(score_prediction(real_fvc, pred_fvc, conf))
+
+            # Calc and add the patient's average prediction score for all slices
+            patient_avg_score = np.mean(patient_scores)
+            conf_scores.append(patient_avg_score)
+            if verbose:
+                print(
+                    f"Mean prediction score for patient {pid} at conf % {conf}: {patient_avg_score}"
+                )
+
+        # Calc and add the overall average prediction score for the current conf percentile
+        avg_conf_score = np.mean(conf_scores)
+        average_scores.append(avg_conf_score)
+        if verbose:
+            print(
+                f"Mean prediction score for all patients at conf % {conf}: {avg_conf_score}"
+            )
+
+    # Find the confidence with the best avg score for all patients - higher is better for LPL
+    best_score_idx = (np.argmax(average_scores) + 1)
+    best_confidence = conf_band_size * best_score_idx
+    if verbose:
+        print(f"Confidence with the highest average prediction score: {best_confidence}")
+
+    return best_confidence
